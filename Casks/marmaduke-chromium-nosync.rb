69befd6fe172fcce659154d8c0d5d4e69892b157
Charl P. Botha <cpbotha@vxlabs.com>
2019-03-05 12:57:10 +0200

    Update version of sync and nosync to 72.0.3626.121

--- Casks/marmaduke-chromium-nosync.rb
+++ Casks/marmaduke-chromium-nosync.rb
@@ -1,9 +1,9 @@
 cask 'marmaduke-chromium-nosync' do
-  version '72.0.3626.81'
-  sha256 '92990cf4184e0a83a12ba767874577d64ea3acbb7ea770ff92fb2c077651731e'
+  version '72.0.3626.121'
+  sha256 'b2aea2f52d8d51a3a5325061b53e043ce54be551c0ef775333f7cdf5ce5044f9'
 
   # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r612437-macOS/Chromium.#{version}.nosync.app.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r612437-macOS/Chromium.#{version}.app.nosync.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
--- Casks/marmaduke-chromium.rb
+++ Casks/marmaduke-chromium.rb
@@ -1,9 +1,9 @@
 cask 'marmaduke-chromium' do
-  version '72.0.3626.81'
-  sha256 '5f894e8181d365494b57d33c58b651b0d2817bf88c0e10c01c8f13c7cd4c046c'
+  version '72.0.3626.121'
+  sha256 '3e7c2af4a8ffb9b4831a5fabe561dfb384d4cd42384c7532621f414a8b14400a'
 
   # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r612437-macOS/Chromium.#{version}.sync.app.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r612437-macOS/Chromium.#{version}.app.sync.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
