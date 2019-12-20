211a79c03fd8c6c81938afaf1e3775761277b811
Matthew Salazar <m@tthewsalazar.com>
2019-12-20 17:15:02 -0500

    Fixed SHA256 for all casks

--- Casks/marmaduke-chromium-nosync.rb
+++ Casks/marmaduke-chromium-nosync.rb
@@ -1,7 +1,7 @@
 cask 'marmaduke-chromium-nosync' do
   version '79.0.3945.88'
   revision '706915'
-  sha1 '350936433CCE3FDEE0B781DD346392EC164D16FD'
+  sha256 '634fe6879e2f15ee627174d92aea1de6877bf126eef2b20927d15d85329cdc1d'
   
   # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
   url "https://github.com/macchrome/macstable/releases/download/v#{version}-r#{revision}-macOS/Chromium.#{version}.nosync.app.zip"
--- Casks/marmaduke-chromium-ungoogled.rb
+++ Casks/marmaduke-chromium-ungoogled.rb
@@ -1,7 +1,7 @@
 cask 'marmaduke-chromium-ungoogled' do
   version '79.0.3945.88'
   revision '706915'
-  sha1 '7328A3227D0FF22E844A839D5E22F58D86188E64'
+  sha256 '6157da29da105c6b4b32fd909e71f7c6f524c972d17dd7e53b2ba775e6d831a8'
 
   # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
   url "https://github.com/macchrome/macstable/releases/download/v#{version}-r#{revision}-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
