f8888e6c6adaef50ec914ca1466ea2be5480f266
Matthew Salazar <m@tthewsalazar.com>
2020-01-15 12:14:41 -0500

    Fixed URLs for sync/nosync (they seem to be typos in latest release)

--- Casks/marmaduke-chromium-nosync.rb
+++ Casks/marmaduke-chromium-nosync.rb
@@ -2,7 +2,9 @@ cask 'marmaduke-chromium-nosync' do
   version '79.0.3945.117'
   sha256 'efa1af82746a940b862b4a475ccade411ce4d54260e344c3284ed486e5e56474'
 
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r706915-macOS/Chromium.#{version}.nosync.app.zip"
+  # URL changed; not sure if permanent or typo.
+  # url "https://github.com/macchrome/macstable/releases/download/v#{version}-r706915-macOS/Chromium.#{version}.nosync.app.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r706915-macOS945.117-r706915-macOS/Chromium.#{version}.nosync.app.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
--- Casks/marmaduke-chromium.rb
+++ Casks/marmaduke-chromium.rb
@@ -2,7 +2,9 @@ cask 'marmaduke-chromium' do
   version '79.0.3945.117'
   sha256 '998b5d55ead10358d5eaa0ce957fea4ffde9b264cdc93e16be122e5c80e36bd5'
 
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r706915-macOS/Chromium.#{version}.sync.app.zip"
+  # URL changed; not sure if permanent or typo.
+  # url "https://github.com/macchrome/macstable/releases/download/v#{version}-r706915-macOS/Chromium.#{version}.sync.app.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r706915-macOS945.117-r706915-macOS/Chromium.#{version}.sync.app.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
