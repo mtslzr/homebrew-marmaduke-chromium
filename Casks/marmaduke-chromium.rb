47063740fdb2f698d6837bf960a83a6650d257a4
Matthew Salazar <m@tthewsalazar.com>
2020-03-20 12:27:13 -0400

    Updated marmaduke-chromium to 80.0.3987.149

--- Casks/marmaduke-chromium.rb
+++ Casks/marmaduke-chromium.rb
@@ -1,8 +1,8 @@
 cask 'marmaduke-chromium' do
-  version '80.0.3987.116'
-  sha256 '1ef76ab1d51464cda672eb65a4c89d6d7985d08f18c26994147645fafd9f230f'
+  version '80.0.3987.149'
+  sha256 '3d5fdaefc2375318a1965a09000a9e7e67feed4bd5f5a9ce880e037572389ea8'
 
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r722274-macOS/Chromium.#{version}.sync.app.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r989-macOS/Chromium.#{version}.sync.app.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
