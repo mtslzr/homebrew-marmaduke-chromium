3cf904505d1205e56b3c5d8aa6e3ad91fccb2c0a
Matthew Salazar <m@tthewsalazar.com>
2020-04-08 12:49:51 -0400

    Updated nosync to 81.0.4044.92

--- Casks/marmaduke-chromium-nosync.rb
+++ Casks/marmaduke-chromium-nosync.rb
@@ -1,8 +1,8 @@
 cask 'marmaduke-chromium-nosync' do
-  version '80.0.3987.149'
-  sha256 'd14d5884e10ebc7eea1008a82da367860e6b5ce9c07706ac1e932e20158b92b3'
+  version '81.0.4044.92'
+  sha256 '2e5287dca58f27fa4844592057515893a20397519859c9daa5fc9834e6b6596b'
 
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r989-macOS/Chromium.#{version}.nosync.app.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r737173-macOS/Chromium.#{version}.nosync.app.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
