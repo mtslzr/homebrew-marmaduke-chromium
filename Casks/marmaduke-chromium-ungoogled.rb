4cc53d050357e2dca34931235c4d82337914de82
Matthew Salazar <m@tthewsalazar.com>
2020-07-16 21:46:03 -0400

    Update Ungoogled to 84.0.4147.89

--- Casks/marmaduke-chromium-ungoogled.rb
+++ Casks/marmaduke-chromium-ungoogled.rb
@@ -1,8 +1,8 @@
 cask 'marmaduke-chromium-ungoogled' do
-  version '83.0.4103.116'
-  sha256 'e7d6be4fe5dfe989adf65f92d0d64a1f93efb41693a7b89a6602a3d31f431979'
+  version '84.0.4147.89'
+  sha256 'f2117dc79c51b997f57464b53593e6bbfa914bd9f3e2eb58891f33016c578e7c'
 
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r756066-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r768962-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
