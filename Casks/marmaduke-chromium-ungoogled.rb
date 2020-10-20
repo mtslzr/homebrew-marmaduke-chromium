33bd5f5d5758b9bfcfbf43d7b3473042118a319d
Matthew Salazar <m@tthewsalazar.com>
2020-10-20 18:15:56 +0000

    Update sync/Ungoogled to 86.0.4240.75

--- Casks/marmaduke-chromium-ungoogled.rb
+++ Casks/marmaduke-chromium-ungoogled.rb
@@ -1,8 +1,8 @@
 cask 'marmaduke-chromium-ungoogled' do
-  version '85.0.4183.102'
-  sha256 '9770c11fd3ae74142ebd07d1913be23fb29fcee18a4fe61ab6365c558b93119a'
+  version '86.0.4240.80'
+  sha256 '6144F509E67A019A20E2FB54A8341788161C9F9DF15BD893A3DCC1414AF65E27'
 
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r782793-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r800218-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
--- Casks/marmaduke-chromium.rb
+++ Casks/marmaduke-chromium.rb
@@ -1,6 +1,6 @@
 cask 'marmaduke-chromium' do
-  version '85.0.4183.102'
-  sha256 '8cd1e55b34171826d02bd16586ac49190992d8bfeac0d6e776b106221da0f5e0'
+  version '86.0.4240.75'
+  sha256 'E3C24C7D95C6237ED06AA7B90E7FA22E2C3CD4505A23B63FB152698E63A8F5E5'
 
   url "https://github.com/macchrome/macstable/releases/download/v#{version}-r782793-macOS/Chromium.#{version}.sync.app.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
