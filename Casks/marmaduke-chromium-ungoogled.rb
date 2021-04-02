08061d115b6a1c98fc8a73c7c608ec75e85d8add
GitHub Action <action@github.com>
2021-04-02 21:23:25 +0000

    Update tap

--- Casks/marmaduke-chromium-ungoogled.rb
+++ Casks/marmaduke-chromium-ungoogled.rb
@@ -1,6 +1,6 @@
 cask 'marmaduke-chromium-ungoogled' do
-  version '89.0.4389.90'
-  sha256 'a377767e97f74b72efb68f1952c98db08582860d0431b7ce6e27f40f7b233dfc'
+  version '89.0.4389.114'
+  sha256 'a7b9f4ba39672b75b15d9348f50784fe912d0054ee9fdc00d01401da189eb30d'
 
   url "https://github.com/macchrome/macstable/releases/download/v#{version}-r843830-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
--- Casks/marmaduke-chromium.rb
+++ Casks/marmaduke-chromium.rb
@@ -1,6 +1,6 @@
 cask 'marmaduke-chromium' do
-  version '89.0.4389.90'
-  sha256 '61a34d7d525e15bbcb7a196cc0b6233465a7d0b1e754bd17dbaae0b2e274fb6a'
+  version '89.0.4389.114'
+  sha256 'a8ee2eaa1459c730984ea25648f171ea230b76cce0647eb7e4cb0d5db2dc1ef8'
 
   url "https://github.com/macchrome/macstable/releases/download/v#{version}-r843830-macOS/Chromium.#{version}.sync.app.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
