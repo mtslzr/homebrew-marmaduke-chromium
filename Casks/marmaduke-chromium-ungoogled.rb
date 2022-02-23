f27377d44ddfc2495cf82bf2018bc8b380a2d423
GitHub Action <action@github.com>
2022-02-23 21:10:33 +0000

    Update tap

--- Casks/marmaduke-chromium-ungoogled.rb
+++ Casks/marmaduke-chromium-ungoogled.rb
@@ -1,6 +1,6 @@
 cask 'marmaduke-chromium-ungoogled' do
-  version '98.0.4758.102'
-  sha256 'e7309b8c5e075087918e0b3496947a55039b7a831de735fb5b4f9898c0b40099'
+  version '98.0.4758.82'
+  sha256 '10c5494ec77909aa723fdbb1530fa36e2fca78b93b2e9772be123189bd0a6510'
 
   url "https://github.com/macchrome/macstable/releases/download/v#{version}-r950365-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Builds are pulled from [macchrome/macstable](https://github.com/macchrome/macsta
 
 ![](https://img.shields.io/badge/marmaduke--chromium--nosync-84.0.4147.89%20(768962)-lightblue)
 
-![](https://img.shields.io/badge/marmaduke--chromium--ungoogled-98.0.4758.102%20(950365)-yellow)
+![](https://img.shields.io/badge/marmaduke--chromium--ungoogled-98.0.4758.82%20(950365)-yellow)
 
 ## Installation
 
