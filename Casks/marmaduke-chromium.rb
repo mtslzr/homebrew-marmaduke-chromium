5e908260ba8fc71b9449cdf59d01948e0643f2d0
GitHub Action <action@github.com>
2023-07-02 21:05:29 +0000

    Update tap

--- Casks/marmaduke-chromium.rb
+++ Casks/marmaduke-chromium.rb
@@ -1,8 +1,8 @@
 cask 'marmaduke-chromium' do
-  version '114.5735.53'
-  sha256 '28c024f1f18bde7014ab43ed86f50b31f90b38d2cbd7b9d744e9da25e21e603c'
+  version '115.5790.60'
+  sha256 '7290f61bfb05bb57b160308cee3b7c3a9f496209e67725319c7ae3dca274f122'
 
-  url 'https://github.com/macchrome/macstable/releases/download/v114.5735.53-M114.0.5735.53-r1135570-macOS/Chromium.app.sync-114.0.5735.53.tar.xz'
+  url 'https://github.com/macchrome/macstable/releases/download/v115.5790.60-M115.0.5790.60-r1148114-macOS/Chromium.app.sync-115.0.5790.60.tar.xz'
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
