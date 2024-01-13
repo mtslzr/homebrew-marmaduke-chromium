04a52b10cea7f3d226e841a3c3e34c1825597f72
GitHub Action <action@github.com>
2024-01-13 21:07:48 +0000

    Update tap

--- Casks/marmaduke-chromium-ungoogled.rb
+++ Casks/marmaduke-chromium-ungoogled.rb
@@ -1,8 +1,8 @@
 cask 'marmaduke-chromium-ungoogled' do
-  version '120.6099.180'
-  sha256 '37b2e1f22c193b68c21a3f10bf8c99f95327803cc19d2eae132185088a88203c'
+  version '120.6099.234'
+  sha256 'af228f793e946f033088728341f7d1a742828f8783faf81698d7cbc3ff8edbb0'
 
-  url 'https://github.com/macchrome/macstable/releases/download/v120.6099.180-M120.0.6099.180-r1217362-macOS/Chromium.app.ungoogled-120.0.6099.180.tar.xz'
+  url 'https://github.com/macchrome/macstable/releases/download/v120.6099.234-M120.0.6099.234-r1217362-macOS/Chromium.app.ungoogled-120.0.6099.234.tar.xz'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
 
