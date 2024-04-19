c8d1fcf1ade95f8ae7bde7117ddbbb56a71823c5
Shadowlee <S0urceC0der@users.noreply.github.com>
2024-04-19 17:30:02 +0800

    Update marmaduke-chromium-nosync.rb
    fix homebrew error:  Calling the `appcast` stanza is disabled! Use the `livecheck` stanza instead.

--- Casks/marmaduke-chromium-nosync.rb
+++ Casks/marmaduke-chromium-nosync.rb
@@ -3,7 +3,6 @@ cask 'marmaduke-chromium-nosync' do
   sha256 '1fcb785f59b201962dfe88aab7f48a80a5d292acb53ea25298f0cae0f2b1b859'
 
   url "https://github.com/macchrome/macstable/releases/download/v#{version}-r768962-macOS/Chromium.#{version}.nosync.app.zip"
-  appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
 
